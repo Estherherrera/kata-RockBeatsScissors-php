2e90d29158fb4ae544e8bebd0087151a839adf8c
Estherherrera <graxyherrera@gmail.com>
2022-08-23 08:58:58 +0200

    test de competitor

---
+++ tests/CompetitorTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Competitor;
+use App\Models\Paper;
+use App\Models\Rock;
+use App\Models\Scissors;
+use PHPUnit\Framework\TestCase;
+
+class CompetitorTest extends TestCase
+{
+    public function test_competitor_choice_Rock(){
+        
+    }
+}
